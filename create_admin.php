c6f48720c01fb0c030413b43ae09017d29a54aa0
mariahsantander <130644356+SunThunderz@users.noreply.github.com>
2025-04-25 22:16:50 +0800

    Added comment for instructions

--- create_admin.php
+++ create_admin.php
@@ -8,10 +8,12 @@ if ($conn->connect_error) {
 $username = "username"; // put your desired username here
 $password = password_hash("password", PASSWORD_DEFAULT); // put your desired password here
 
-// After putting your desired username and password
+// After putting your desired username and password for admin
 // Open your browser and visit
 // http://localhost/portfolio/create_admin.php
 // To creatde admin account
+// It should display "Admin user created successfully."
+// Delete this file after creating the admin account
 
 $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
 $stmt->bind_param("ss", $username, $password);
